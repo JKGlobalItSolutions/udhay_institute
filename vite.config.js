bafd3576375b432eaaba15054870234b792c6b43
sriram A <sriram.coder2004@gmail.com>
2025-07-01 19:54:18 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/udhay_institute/',
+  base: './', // ✅ Use this if deploying at root (e.g., https://yourdomain.com/)
   plugins: [react()],
 });
